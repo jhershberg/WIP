f4efb33697525d51fbe2e026d9660e1de15e3054
jhershbe@redhat.comn <jhershbe@redhat.comn>
2017-06-08 16:21:03 +0000

    scripts, untested

---
+++ buildnets.sh
@@ -0,0 +1,51 @@
+action=c
+num_nets=5
+num_subnets=5
+num_ports=20
+
+
+for net_num in $(seq 0 $(expr $num_nets - 1))
+do
+	net_name=net.${net_num}
+
+	if [ $action = c ]
+	then
+		neutron net-create $net_name || exit 1
+	fi
+
+	for sub_num in $(seq 0 $(expr $num_subnets - 1))
+	do
+		sub_name=sub.${net_num}.${sub_num}
+		pool_start=10.${net_num}.${sub_num}.100
+		pool_end=10.${net_num}.${sub_num}.200
+		cidr=10.${net_num}.${sub_num}.0/24
+
+		if [ $action = c ]
+		then
+			neutron subnet-create ${net_name} --name ${sub_name} --allocation-pool start=${pool_start},end=${pool_end} $cidr || exit 1
+		fi
+
+		for port_num in $(seq 1 $num_ports)
+		do
+			ip=10.${net_num}.${sub_num}.$(expr 100 + $port_num)
+
+			if [ $action = c ]
+			then
+				./mkport.sh $net_name $sub_name $ip || exit 1
+			fi
+
+			if [ $action = d ]
+			then
+				./delport.sh $ip
+			fi
+		done
+		if [ $action = d ]
+		then
+			neutron subnet-delete $sub_name
+		fi
+	done
+	if [ $action = d ]
+	then
+		neutron net-delete $net_name
+	fi
+done
--- mkport.sh
+++ mkport.sh
@@ -6,9 +6,10 @@ network=$1
 subnet_id=$(neutron subnet-list | grep $subnet | awk '{print $2}')
 mac=$(uuidgen | md5sum | sed -e 's/\(..\)\(..\)\(..\)\(..\).*/52:54:\1:\2:\3:\4/')
 
-neutron port-create --name port-${ip} --fixed-ip subnet_id=$(subnet_id),ip_address=${ip} $network --mac-address $mac --device-owner compute:nova --device-id $(uuidgen)
+neutron port-create --name port-${ip} --fixed-ip subnet_id=$(subnet_id),ip_address=${ip} $network --mac-address $mac --device-owner compute:nova --device-id $(uuidgen)  || exit 1
 
 port_uuid=$(neutron port-list | grep $mac | awk '{print $2}')
 tap_name=$(echo $port_uuid | sed -e 's/\(...........\).*/tap\1/')
 
 sudo ovs-vsctl add-port br-int $tap_name -- set interface $tap_name type=internal -- set interface $tap_name external_ids=attached_mac=\"$mac\",iface-id=$port_uuid,iface-status=active
+exir $?
